d1970ba4532fafd7abf224d20970414608372ec9
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2023-12-10 18:13:02 -0500

    feat: Refined complex number struct, add lockfile

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
+#![allow(dead_code)]
+use std::fmt;
+use creusot_contracts::*;
+
+type Number = f64;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 struct Complex {
-    re: f64,
-    im: f64,
+    re: Number,
+    im: Number,
+}
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let operator = if Number::signum(self.im) == 1.0 {
+            "+"
+        } else {
+            "-"
+        };
+        write!(f, "{} {} {}i", self.re, operator, self.im.abs())
+    }
 }
 
 impl Complex {
-    fn new(re: f64, im: f64) -> Complex {
+    fn new(re: Number, im: Number) -> Complex {
         Complex { re, im }
     }
-    fn add(&self, other: &Complex) -> Complex {
+    fn mult(&self, other: &Complex) -> Complex {
+        Complex {
+            re: self.re * other.re - self.im * other.im,
+            im: self.re * other.im + self.im * other.re,
+        }
+    }
+    fn plus(&self, other: &Complex) -> Complex {
         Complex {
             re: self.re + other.re,
             im: self.im + other.im,
@@ -20,13 +42,16 @@ impl Complex {
             im: -self.im,
         }
     }
-    fn norm(&self) -> f64 {
+    fn norm(&self) -> Number {
         (self.re * self.re + self.im * self.im).sqrt()
     }
 }
 
 fn main() {
-    println!("Hello world");
+    let a = Complex::new(1.0, 1.0);
+    let b = Complex::new(1.0, -1.0);
+    println!("a = {}", a);
+    println!("b = {}", b);
 }
 
 #[cfg(test)]
@@ -37,6 +62,6 @@ mod tests {
     fn test_add() {
         let a = Complex::new(1.0, 1.0);
         let i = Complex::new(0.0, 0.0);
-        assert_eq!(a.add(&i), a);
+        assert_eq!(a.plus(&i), a);
     }
 }
